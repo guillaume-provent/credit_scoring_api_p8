326fc823271653e289b91b9145cf3898816e0d3b
Guillaume Provent <guillaume.provent@gmail.com>
2024-06-26 20:36:14 +0200

    Add test_api.py

---
+++ test_api.py
@@ -0,0 +1,36 @@
+import pytest
+import joblib
+import pandas as pd
+from api import app, model_path, data_path
+
+
+# Configuration de pytest pour utiliser le client de test de Flask :
+@pytest.fixture
+def client():
+
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+# Test de chargement du modèle :
+def test_model_loading():
+
+    model = joblib.load(model_path)
+    assert model is not None
+
+
+# Test de la prédiction :
+def test_predict_valid_id(client):
+
+    data = {'AMT_GOODS_PRICE': 100, 'AMT_ANNUITY': 20, 'AMT_CREDIT'=100}
+
+    response = client.post('/predict', json=data)
+    json_data = response.get_json()
+
+    assert response.status_code == 200
+    assert 'Dossier' in json_data
+    assert 'Probabilite' in json_data
+    assert 'Seuil' in json_data
+    assert 'Shapvals' in json_data
+    assert 'Basevals' in json_data
